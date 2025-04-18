24bac1ba94112fbd7ce535981010f13c8ce956e1
Pumafi <ferdinand.bhavsar@epita.fr>
2025-04-18 17:35:50 +0200

    Added glorot uniform weights initialization

---
+++ weights_init.h
@@ -0,0 +1,31 @@
+# pragma once
+
+# include <vector>
+# include <random>
+# include <cmath>
+# include <algorithm>
+
+float glorot_uniform_values(int input_dim, int output_dim){
+    std::random_device rd;
+    std::mt19937 gen(rd());
+
+    float lower_limit = -std::sqrt(6) / std::sqrt(input_dim + output_dim);
+    float higher_limit = std::sqrt(6) / std::sqrt(input_dim + output_dim);
+
+    std::uniform_real_distribution<float> distribution(lower_limit, higher_limit);
+
+    return distribution(gen);
+}
+
+std::vector<float> vector_glorot_uniform_init(int input_dim, int output_dim){
+    std::vector<float> output(output_dim);
+    std::transform(output.begin(), output.end(), output.begin(), [&](float){return glorot_uniform_values(input_dim, output_dim); });
+
+    return output;
+}
+
+std::vector<std::vector<float>> matrix_2d_glorot_uniform_init(int input_dim, int output_dim){
+    std::vector<std::vector<float>> output(input_dim);
+    std::transform(output.begin(), output.end(), output.begin(), [&](std::vector<float>){return vector_glorot_uniform_init(input_dim, output_dim); });
+    return output;
+}
