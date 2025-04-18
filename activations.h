7291dc90377c0d23a84f9a62d2839104bb9b3aa4
Pumafi <ferdinand.bhavsar@epita.fr>
2025-04-18 15:43:45 +0200

    Added the activation function class
    Created activations.h file, with abstract class
    and derived classes for some activation functions.

---
+++ activations.h
@@ -0,0 +1,92 @@
+# pragma once
+
+# include <iostream>
+# include <vector>
+# include <cmath>
+# include <algorithm>
+# include <numeric>
+
+class Activation{
+    public:
+        virtual ~Activation() = default;
+        virtual std::vector<float> call(std::vector<float>) = 0;
+        std::vector<float> get_gradients();
+    protected:
+        std::vector<float> gradients;
+};
+
+std::vector<float> Activation::get_gradients(){
+    std::vector<float> output(gradients);
+    return output;
+}
+
+class IdentityActivation : public Activation{
+    /*
+    Identity activation. Returns the input.
+    */  
+   public:
+    std::vector<float> call(std::vector<float>);   
+};
+
+std::vector<float> IdentityActivation::call(std::vector<float> input){
+    std::vector<float> output(input.begin(), input.end());
+
+    std::transform(input.begin(), input.end(), std::back_inserter(gradients), [](float x){return 1; });    
+
+    return output;
+}
+
+class LogisticActivation : public Activation{   
+    /*
+    Logistic activation function, also called sigmoid function.
+    Simple implementation that applies the function element wise to the input vector and compute gradient.
+    */  
+    public:
+        std::vector<float> call(std::vector<float>);   
+};
+
+std::vector<float> LogisticActivation::call(std::vector<float> input){
+    /*
+    Apply the logistic activation element wise to the input vector.
+    */
+    std::vector<float> output;
+
+    gradients.clear();
+    // Compute gradient in the forward pass (TODO option to not do that)
+    std::transform(input.begin(), input.end(), std::back_inserter(gradients), [](float x){return (1 / (1 + std::exp(-x))) * (1 - 1 / (1 + std::exp(-x))); });
+
+    // Compute output
+    std::transform(input.begin(), input.end(), std::back_inserter(output), [](float x){return 1 / (1 + std::exp(-x)); });
+
+    return output;
+}
+
+class SoftmaxActivation : public Activation{
+    /*
+    Softmax activation function.
+    */
+    public:
+        std::vector<float> call(std::vector<float>);   
+};
+
+std::vector<float> SoftmaxActivation::call(std::vector<float> input){
+
+    // we substract the max value in the input vector for computational stability
+    float max_input_val = *std::max_element(input.begin(), input.end());
+
+    std::vector<float> exp_input;
+
+    std::transform(input.begin(), input.end(), std::back_inserter(exp_input), [max_input_val](float x){return std::exp(x - max_input_val); });
+
+    float sum_exp_input = std::accumulate(exp_input.begin(), exp_input.end(), 0.);
+
+    std::vector<float> output;
+
+    std::transform(exp_input.begin(), exp_input.end(), std::back_inserter(output), [sum_exp_input](float x){return x / sum_exp_input; });
+
+    gradients.clear();
+    // I am not really computing the gradient, our gradient will be zero everywhere and we will use the cross entropy, but this will give the right size
+    std::transform(exp_input.begin(), exp_input.end(), std::back_inserter(gradients), [](float x){return 1; });
+
+    return output;
+}
