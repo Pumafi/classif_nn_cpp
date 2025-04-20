eed364094cfdde47ace8e8d17e1b95122cf84ce6
Pumafi <ferdinand.bhavsar@epita.fr>
2025-04-20 14:48:22 +0200

    Added a few linear algebra functions
    Added vector element wise multiplication
    Added vector outer product
    Added a few matrix operations

--- linear_algebra.h
+++ linear_algebra.h
@@ -2,9 +2,28 @@
 
 # include <vector>
 # include <stdexcept>
+# include <algorithm>
 # include <iostream>
 
-std::vector<float> vector_addition(std::vector<float> vector_a, std::vector<float> vector_b){
+std::vector<float> vector_scalar_multiplication(const float scalar, const std::vector<float> vector_a){
+    std::vector<float> output;
+    std::transform(vector_a.begin(), vector_a.end(), std::back_inserter(output), [&](float x){return x * scalar; });
+
+    return output;
+}
+
+std::vector<float>  element_wise_vector_multiplication(const std::vector<float> vector_a, const std::vector<float> vector_b){
+    if (vector_a.size() != vector_b.size()){
+        throw std::invalid_argument("vectors need to be the same size for scalar product!");
+    }
+    std::vector<float> output(vector_a.size());
+    for (int i = 0; i < vector_a.size(); ++i){
+        output[i] = vector_a[i] * vector_b[i];
+    }
+    return output;
+}
+
+std::vector<float> vector_addition(const std::vector<float> vector_a, const std::vector<float> vector_b){
     if (vector_a.size() != vector_b.size()){
         throw std::invalid_argument("vectors need to be the same size for addition!");
     }
@@ -15,7 +34,7 @@ std::vector<float> vector_addition(std::vector<float> vector_a, std::vector<floa
     return output;
 }
 
-float vector_scalar_product(std::vector<float> vector_a, std::vector<float> vector_b){
+float vector_scalar_product(const std::vector<float> vector_a, const std::vector<float> vector_b){
     if (vector_a.size() != vector_b.size()){
         throw std::invalid_argument("vectors need to be the same size for scalar product!");
     }
@@ -26,10 +45,10 @@ float vector_scalar_product(std::vector<float> vector_a, std::vector<float> vect
     return output;
 }
 
-std::vector<float> vector_matrix_multiplication(std::vector<float> vector, std::vector<std::vector<float>> matrix){
+std::vector<float> vector_matrix_multiplication(const std::vector<float> vector, const std::vector<std::vector<float>> matrix){
     if (vector.size() != matrix.size()){
-        std::cout << "ERROR: invalid shapes" << std::endl;
-        std::cout << "ERROR: invalid shapes. Size Matrix:" << matrix.size() << "Size vector: " << vector.size() << std::endl;
+        std::cerr << "ERROR: invalid shapes" << std::endl;
+        std::cerr << "ERROR: invalid shapes. Size Matrix:" << matrix.size() << "Size vector: " << vector.size() << std::endl;
         throw std::invalid_argument("matrix must have same first dimensiosn as vector for scalar product!");
     }
 
@@ -44,7 +63,7 @@ std::vector<float> vector_matrix_multiplication(std::vector<float> vector, std::
     return output;
 }
 
-std::vector<std::vector<float>> matrix_transpose(std::vector<std::vector<float>> matrix){
+std::vector<std::vector<float>> matrix_transpose(const std::vector<std::vector<float>> matrix){
     std::vector<std::vector<float>> output(matrix[0].size(), std::vector<float>(matrix.size()));
     for (int i =0; i < matrix.size(); ++i){
         for (int j = 0; j < matrix[0].size(); ++j){
@@ -54,7 +73,7 @@ std::vector<std::vector<float>> matrix_transpose(std::vector<std::vector<float>>
     return output;
 }
 
-std::vector<std::vector<float>> outer_product(std::vector<float> vector_a, std::vector<float> vector_b) {
+std::vector<std::vector<float>> outer_product(const std::vector<float> vector_a, const std::vector<float> vector_b) {
     std::vector<std::vector<float>> output(vector_a.size(), std::vector<float>(vector_b.size()));
 
     for (size_t i = 0; i < vector_a.size(); ++i) {
@@ -63,5 +82,21 @@ std::vector<std::vector<float>> outer_product(std::vector<float> vector_a, std::
         }
     }
 
+    return output;
+}
+
+std::vector<std::vector<float>> matrix_addition(const std::vector<std::vector<float>> matrix_a, const std::vector<std::vector<float>> matrix_b) {
+    if (matrix_a.size() != matrix_b.size()){
+        throw std::invalid_argument("Outer dimension must be the same size for mat addition!");
+    }
+    if (matrix_a[0].size() != matrix_b[0].size()){
+        throw std::invalid_argument("Inner dimension must be the same size for mat addition!");
+    }
+    std::vector<std::vector<float>> output(matrix_a.size(), std::vector<float>(matrix_a[0].size()));
+    for (int i = 0; i < matrix_a.size(); ++i){
+        for (int j = 0; j < matrix_a[0].size(); ++j){
+            output[i][j] = matrix_a[i][j] + matrix_b[i][j];
+        }
+    }
     return output;
 }
